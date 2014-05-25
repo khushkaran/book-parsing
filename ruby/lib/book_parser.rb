3c1d95c972e62193cd235f1a07c52af361efbaee
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-25 20:35:28 +0100

    Can count occurances of word

--- ruby/lib/book_parser.rb
+++ ruby/lib/book_parser.rb
@@ -9,4 +9,12 @@ class BookParser
     string.gsub!(/[^\w\s]/, "")
     string.split(" ")
   end
+
+  def occurences(string)
+    counts = Hash.new 0
+    string.each do |word|
+      counts[word] += 1
+    end
+    counts
+  end
 end
--- ruby/spec/book_parser_spec.rb
+++ ruby/spec/book_parser_spec.rb
@@ -12,15 +12,15 @@ describe BookParser do
   context "Splitting a string" do
 
     let(:string) { "This is simples! This will be fun!" }
+    let(:split_string) { ["This", "is", "simples", "This", "will", "be", "fun"] }
 
     it "can split a string" do
-      sample_array = ["This", "is", "simples", "This", "will", "be", "fun"]
-      expect(parser.split(string)).to eq sample_array
+      expect(parser.split(string)).to eq split_string
     end
 
     it "can count the occurences of words" do
       occurences = {"This"=>2, "is"=>1, "simples"=>1, "will"=>1, "be"=>1, "fun"=>1}
-      expect(parser.occurences(string)).to eq occurences
+      expect(parser.occurences(split_string)).to eq occurences
     end
   end
   
