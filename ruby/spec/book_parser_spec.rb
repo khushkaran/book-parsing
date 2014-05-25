689bfc1f2dd8beea43386f75beaace1a601a8c8e
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-25 20:31:09 +0100

    Added test for occurences

--- ruby/spec/book_parser_spec.rb
+++ ruby/spec/book_parser_spec.rb
@@ -10,10 +10,17 @@ describe BookParser do
   end
 
   context "Splitting a string" do
+
+    let(:string) { "This is simples! This will be fun!" }
+
     it "can split a string" do
-      sample_string = "This is simples! This will be fun!"
       sample_array = ["This", "is", "simples", "This", "will", "be", "fun"]
-      expect(parser.split(sample_string)).to eq sample_array
+      expect(parser.split(string)).to eq sample_array
+    end
+
+    it "can count the occurences of words" do
+      occurences = {"This"=>2, "is"=>1, "simples"=>1, "will"=>1, "be"=>1, "fun"=>1}
+      expect(parser.occurences(string)).to eq occurences
     end
   end
   
