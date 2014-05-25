6af7b0c1508267f317cb23a306230d50fb18715e
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-25 20:44:38 +0100

    Checked edge case for newlines

--- ruby/spec/book_parser_spec.rb
+++ ruby/spec/book_parser_spec.rb
@@ -22,6 +22,14 @@ describe BookParser do
       occurences = {"This"=>2, "is"=>1, "simples"=>1, "will"=>1, "be"=>1, "fun"=>1}
       expect(parser.occurences(split_string)).to eq occurences
     end
+
+    it "replaces new lines with spaces" do
+      another_string = "This is simples! This will be fun!\nThis is simples! This will be fun!"
+      another_split_string = ["This", "is", "simples", "This", "will", "be", "fun", "This", "is", "simples", "This", "will", "be", "fun"]
+      occurences = {"This"=>4, "is"=>2, "simples"=>2, "will"=>2, "be"=>2, "fun"=>2}
+      expect(parser.split(another_string)).to eq another_split_string
+      expect(parser.occurences(another_split_string)).to eq occurences
+    end
   end
   
 end
