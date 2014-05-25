5fd5f6e5e84114788a3bfbe3d38929c4352de727
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-25 20:01:02 +0100

    Parser can be initialised with text file

--- ruby/lib/book_parser.rb
+++ ruby/lib/book_parser.rb
@@ -1,2 +1,7 @@
 class BookParser
+  attr_reader :book
+
+  def initialize(textfile)
+    @book = File.read(textfile)
+  end
 end
--- ruby/spec/book_parser_spec.rb
+++ ruby/spec/book_parser_spec.rb
@@ -2,5 +2,11 @@ require "spec_helper"
 require "book_parser"
 
 describe BookParser do
+
+  let(:parser) { BookParser.new("../text.txt")}
+
+  it "can be initialised with text file" do
+    expect(parser.book).not_to be_nil
+  end
   
 end
