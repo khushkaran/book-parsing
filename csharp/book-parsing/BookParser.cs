399ce29079786b2c859fab037a73a1d54b69c82e
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-26 10:17:26 +0100

    Ignoring build files, added test class and book parser class

---
+++ csharp/book-parsing/BookParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace bookparsing
+{
+	public class BookParser
+	{
+		public static void Main()
+		{
+			Console.WriteLine("Yo!");
+		}
+	}
+}
+
---
+++ csharp/book-parsing/BookParserTestClass.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace bookparsing
+{
+	[TestFixture ()]
+	public class BookParserTestClass
+	{
+		[Test ()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
