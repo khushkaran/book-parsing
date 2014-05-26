187d3ed1ae07e46e25656ea676cc7e693acb569c
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-05-26 10:46:30 +0100

    Added Test Case for splitting string

--- csharp/book-parsing/BookParserTestClass.cs
+++ csharp/book-parsing/BookParserTestClass.cs
@@ -10,6 +10,11 @@ namespace bookparsing
 		public void LoadBook (){
 			Assert.IsNotEmpty (BookParser.LoadBook("../../../../text.txt"));
 		}
+
+		[Test ()]
+		public void SplitBook (){
+			Assert.AreEqual (61352, BookParser.SplitBook (LoadBook ("../../../../text.txt")).Count);
+		}
 	}
 }
 
